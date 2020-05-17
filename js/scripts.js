4ede4d3d322f50758c37705680ac02d72589cd3c
kelvinmukaria <mukariakelvin17@gmail.com>
2020-05-17 21:38:39 +0300

    Add js functionality and enable toggling between respective descriptions of each icon in the what we do section using jQuery

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $("#imagedesign").click(function(){
+        $("imagedesign").slideDown('1500').hide('1000');
+        $('#design').show('1500');
+    });
+    $('#design').click(function(){
+        $('#design').slideUp('1500');
+        $('#imagedesign').slideDown('1500');
+    });
+    });
+
+$(document).ready(function(){
+    $("#imagedevelopment").click(function(){
+        $("imagedevelopment").slideDown('1500').hide('1000');
+        $('#development').show('1500');
+    });
+    $('#development').click(function(){
+        $('#development').slideUp('1500');
+        $('#imagedevelopment').slideDown('1500');
+    });
+    });
+
+$(document).ready(function(){
+    $("#imageproduct").click(function(){
+        $("imageproduct").slideDown('1500').hide('1000');
+        $('#product').show('1500');
+    });
+    $('#product').click(function(){
+        $('#product').slideUp('1500');
+        $('#imageproduct').slideDown('1500');
+    });
+    });
